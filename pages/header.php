4958d3a2983d5cb33b9ad1c9da719a4586f374bd
Bryleo <bryleo2009@hotmail.com>
2024-04-16 11:56:47 -0500

    favicons

--- pages/header.php
+++ pages/header.php
@@ -1,4 +1,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="#ffffff">
+<meta name="robots" content="index, nofollow">
+<meta http-equiv="cache-control" content="no-cache">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="Spanish">
+<meta name="author" content="Bryan A. Morán Vega">
+<meta name="copyright" content="Of System ®">
+<meta name="theme-color" content="#302b63">
 <script src="https://kit.fontawesome.com/1f84f73194.js"></script>
 <script
 src="https://cdn.lordicon.com/libs/mssddfmo/lord-icon-2.1.0.js"></script>
@@ -11,5 +19,32 @@ rel="stylesheet">
 rel='stylesheet'>
 <link rel="stylesheet"
 href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="icon" href="images/SystemLogo.ico">
 <link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- Apple Touch Icons -->
+<link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="images/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="120x120" href="images/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="152x152" href="images/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-icon-180x180.png">
+
+<!-- Android Icons -->
+<link rel="icon" type="image/png" sizes="36x36" href="images/favicon/android-icon-36x36.png">
+<link rel="icon" type="image/png" sizes="48x48" href="images/favicon/android-icon-48x48.png">
+<link rel="icon" type="image/png" sizes="192x192" href="images/favicon/android-icon-192x192.png">
+
+<!-- Favicon Icons -->
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="images/favicon/favicon-96x96.png">
+
+<!-- Microsoft Icons -->
+<meta name="msapplication-TileImage" content="images/favicon/ms-icon-70x70.png">
+<meta name="msapplication-square70x70logo" content="images/favicon/ms-icon-70x70.png">
+<meta name="msapplication-TileImage" content="images/favicon/ms-icon-144x144.png">
+<meta name="msapplication-square144x144logo" content="images/favicon/ms-icon-144x144.png">
+<meta name="msapplication-square150x150logo" content="images/favicon/ms-icon-150x150.png">
+<meta name="msapplication-wide310x150logo" content="images/favicon/ms-icon-310x150.png">
+<meta name="msapplication-square310x310logo" content="images/favicon/ms-icon-310x310.png">
+
