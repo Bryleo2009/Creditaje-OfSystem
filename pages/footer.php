a3c3633ee2581e2921d5d112ae298b7b586fda6a
Bryleo <bryleo2009@hotmail.com>
2024-05-25 08:20:19 -0500

    corre v3

--- pages/footer.php
+++ pages/footer.php
@@ -1,6 +1,6 @@
-<script src="js/jquery.min.js"></script>
 <script src="js/popper.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/main.js"></script>
 <script src="js/contacto.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
