5f60d3d3e61478389275dec5450f3996ae028f55
Bryleo <bryleo2009@hotmail.com>
2024-05-29 08:10:42 -0500

    chore: Actualizar registro de log con fecha y hora local

--- ofsystem/enviar.php
+++ ofsystem/enviar.php
@@ -111,19 +111,19 @@ if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['service'
     }
 
     $respuesta = $respuesta_reclutador . ' | ' . $respuesta_postulante;
-    $data['log'] = $respuesta;
-    $_POST['data'] = $data;
-    ob_start();
-    include($log);
-    $response = ob_get_clean();
+    // $data['log'] = $respuesta;
+    // $_POST['data'] = $data;
+    // ob_start();
+    // include($log);
+    // $response = ob_get_clean();
     echo $respuesta;
 } else {
     $respuesta = "existen campos vacios" . " nombre:" . $_POST['name'] . " email:" . $_POST['email'] . " servicio:" . $_POST['service'];
-    $data['log'] = $respuesta;
-    $_POST['data'] = $data;
-    ob_start();
-    include($log);
-    $response = ob_get_clean();
+    // $data['log'] = $respuesta;
+    // $_POST['data'] = $data;
+    // ob_start();
+    // include($log);
+    // $response = ob_get_clean();
     echo $respuesta;
 }
 
