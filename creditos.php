dfa0dd798f822953af43dbedc32382a50ffb9b9d
Bryleo <bryleo2009@hotmail.com>
2024-05-29 12:44:28 -0500

    chore: Actualizar encabezados para evitar el caché en creditos.php

--- creditos.php
+++ creditos.php
@@ -1,3 +1,12 @@
+<?php
+// Encabezados para evitar el caché
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+// Incluir el archivo referer.php
+include 'api/referer.php';
+?>
 <!doctype html>
 <html lang="es">
 
