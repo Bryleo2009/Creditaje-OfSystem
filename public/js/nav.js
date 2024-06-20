df32253a38cba456bccac365f2ee1dbeedf68510
Bryleo <bryleo2009@hotmail.com>
2024-06-20 10:13:07 -0500

    feat: Update navigation functionality and styling

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,78 +1,67 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll('.nav-links a');
-  
-    // Función para agregar la clase "active" al elemento de navegación correspondiente
-    const setActiveLink = (id) => {
-        navLinks.forEach(link => {
-            if (link.getAttribute('href') === `#${id}`) {
-                link.classList.add('active');
-            } else {
-                link.classList.remove('active');
-            }
-        });
-    };
-  
-    // Agregar la clase "active" al hacer clic en un enlace
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            navLinks.forEach(link => link.classList.remove('active'));
-            this.classList.add('active');
-        });
-    });
-  
-    // Agregar la clase "active" al desplazarse a una sección
-    window.addEventListener('scroll', function() {
-        const scrollPosition = window.scrollY;
-        document.querySelectorAll('section').forEach(section => {
-            if (
-                scrollPosition >= section.offsetTop - 100 &&
-                scrollPosition < section.offsetTop + section.offsetHeight - 100
-            ) {
-                setActiveLink(section.id);
-            }
-        });
-    });
-});
+  const navLinks = document.querySelectorAll('.nav-links a');
+  const menuToggle = document.getElementById('menu-toggle');
+  const header = document.querySelector("header");
+  const logo = document.getElementById("logo");
+  const serviceLinks = document.querySelectorAll(".service-link");
 
-window.addEventListener("scroll", function () {
-    const nav = document.querySelector("header");
-    const logo = document.getElementById("logo");
+  const setActiveLink = (id) => {
+      navLinks.forEach(link => {
+          link.classList.remove('active');
+          if (link.getAttribute('href') === `#${id}`) {
+              link.classList.add('active');
+          }
+      });
+  };
 
+  const toggleLogoAndNav = () => {
     if (window.scrollY > 113) {
       if (window.innerWidth <= 769) {
         logo.src = "/images/logo/texto-logo-v2.png"; // Cambia al logo v1
       } else {
         logo.src = "/images/logo/texto-logo-v1.png"; // Cambia al logo v2
       }
-      nav.classList.add("nav-scrolled");
+      header.classList.add("nav-scrolled");
+      serviceLinks.forEach(link => link.classList.add("service-link-scrolled"));
     } else {
       logo.src = "/images/logo/texto-logo-v2.png"; // Vuelve al logo original
-      nav.classList.remove("nav-scrolled");
+      //nav.classList.remove("nav-scrolled");
+      header.classList.remove("nav-scrolled");
+      serviceLinks.forEach(link => link.classList.remove("service-link-scrolled"));
     }
+  };
 
-    const serviceLinks = document.querySelectorAll(".service-link");
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 113) {
-        serviceLinks.forEach((link) => {
-          link.classList.add("service-link-scrolled");
-          //link.classList.add('primary');
-        });
-      } else {
-        serviceLinks.forEach((link) => {
-          //link.classList.remove('primary');
-          link.classList.remove("service-link-scrolled");
-        });
-      }
-    });
-  });
+  const handleScroll = () => {
+      toggleLogoAndNav();
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navLinks = document.querySelectorAll('.nav-links a');
-
-    navLinks.forEach(link => {
-      link.addEventListener('click', function() {
-        menuToggle.checked = false;
+      const scrollPosition = window.scrollY;
+      document.querySelectorAll('section').forEach(section => {
+          if (scrollPosition >= section.offsetTop - 100 && scrollPosition < section.offsetTop + section.offsetHeight - 100) {
+              setActiveLink(section.id);
+          }
       });
-    });
+  };
+
+  // Smooth scroll
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+
+          const targetId = this.getAttribute('href').substr(1);
+          const targetSection = document.getElementById(targetId);
+
+          window.scrollTo({
+              top: targetSection.offsetTop - 50,
+              behavior: 'smooth'
+          });
+      });
   });
+
+  window.addEventListener('scroll', handleScroll);
+
+  navLinks.forEach(link => {
+      link.addEventListener('click', function() {
+          menuToggle.checked = false;
+      });
+  });
+});
